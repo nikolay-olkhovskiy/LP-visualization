07f4d32463072a6919cd57a6583b855f4604bf7b
nikolay-olkhovskiy <nikolay.olkhovsky@gmail.com>
2022-02-15 22:07:57 +0500

    Increase receptive field rank and hide visualization scripts.

--- Problem-Parameters.h
+++ Problem-Parameters.h
@@ -13,7 +13,7 @@ This source code is developed based on the BSF skeleton (https://github.com/leon
 #define PP_MAX_M	250									// Maximal  number of inequalities
 #define PP_MAX_N	120									// Maximal  space dimension
 #define PP_ETA		5									// Rank of receptive field 		
-#define PP_DELTA	1									// Density of receptive field
+#define PP_DELTA	5									// Density of receptive field
 #define PP_MAX_K	18446744073709550592ull				// Maximal number of receptive field points
 #define PP_PATH		"C:/Users/Akella/source/repos/nikolay-olkhovskiy/LP-visualization/"	// Working directory of the application
 //#define PP_PATH		"/home/olkhovskiina/"	// Working directory of the application
@@ -23,7 +23,7 @@ This source code is developed based on the BSF skeleton (https://github.com/leon
 #define PP_PICTURE_FILE	"picture.txt"	// File with coordinates suitable to display
 #define PP_RECEPTIVE_FIELD_FILE "receptive_field.txt" // File with coordinates of receptive field points
 #define PP_IMAGE_OUT					// Flag to save list of target distances to the file
-//#define PP_RECEPTIVE_FIELD_OUT			// Flag to save receptive field points to the file
+#define PP_RECEPTIVE_FIELD_OUT			// Flag to save receptive field points to the file
 
 //-------------------------- Macroses ---------------------------
 #define PF_MIN(x,y) (x<y?x:y)
